c0f06f8645bf327ed668f8b16e1a0641161e5616
pentapriya <pentapriya230401@gmail.com>
2022-11-14 15:01:21 +0530

    file to calculate two numbers

--- cal.cpp
+++ cal.cpp
@@ -7,12 +7,31 @@ class cal{
 		x=0;y=0;
 	}
 	int add(int,int);
+	int sub(int,int);
+	int mul(int,int);
+	int div(int,int);
 };
 int cal::add(int x,int y){
 	return x+y;
 }
+int cal::sub(int x,int y){
+        return x-y;
+}
+int cal::mul(int x,int y){
+        return x*y;
+}
+int cal::div(int x,int y){
+        return x/y;
+}
+
 int main(){
 	cal obj;
-	cout<<obj.add(4,8);
+	int a,b;
+	cout<<"enter numbers"<<endl;
+	cin>>a>>b;
+	cout<<"addition: "<<obj.add(a,b)<<endl;
+	cout<<"subtraction: "<<obj.sub(a,b)<<endl;
+	cout<<"product: "<<obj.mul(a,b)<<endl;
+	cout<<"division: "<<obj.div(a,b)<<endl;
 	return 0;
 }
